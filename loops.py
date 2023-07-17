2ab81fc600ade8999c6724052e6dca95c7878e27
Kye Clevenger <Kyeclevenger@gmail.com>
2023-07-17 15:27:16 -0600

    second

--- loops.py
+++ loops.py
@@ -1,14 +1,80 @@
-dojo = {
-   'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
-   'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+# Question One 
+# Update Values in Dictionaries and Lists
+
+x = [ [5,2,3], [10,8,9] ] 
+students = [
+    {'first_name':  'Michael', 'last_name' : 'Jordan'},
+    {'first_name' : 'John', 'last_name' : 'Rosales'}
+]
+sports_directory = {
+    'basketball' : ['Kobe', 'Jordan', 'James', 'Curry'],
+    'soccer' : ['Messi', 'Ronaldo', 'Rooney']
 }
-
-def printinfo(dict):
-
-    for key in dict:
-        print(len(dict[key]),key)
-        for value in dict[key]:
-            print(value)
+z = [ {'x': 10, 'y': 20} ]
+x[1][0] = 15
+students[0]['last_name'] = 'Bryant'
+sports_directory['soccer'][0] = 'Andres'
+z[0]['y'] = 30
 
 
-printinfo(dojo)
+
+# Question Two
+# Iterate Through a List of Dictionaries
+
+students = [
+        {'first_name':  'Michael', 'last_name' : 'Jordan'},
+        {'first_name' : 'John', 'last_name' : 'Rosales'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+def iterateDictionary(some_list = students):
+    for key_name in some_list:
+        print(key_name)
+iterateDictionary(students) 
+
+
+
+# Question Three
+# Get Values From a List of Dictionaries
+
+def iterateDictionary(key_name, some_list):
+    for i in range(0,len(some_list)):
+        print(some_list[i][key_name])
+iterateDictionary('first_name', students)
+
+
+
+# Question Four
+# Iterate Through a Dictionary with List Values
+
+dojo = {
+    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+}
+def printInfo(some_list):
+    for key_name, value in some_list.items():
+        print( f"{len(value)} {key_name.upper()} ")
+        for i in range(0,len(value)):
+            print(value[i])
+printInfo(dojo)
+
+
+# output:
+# 7 LOCATIONS
+# San Jose
+# Seattle
+# Dallas
+# Chicago
+# Tulsa
+# DC
+# Burbank
+
+# 8 INSTRUCTORS
+# Michael
+# Amy
+# Eduardo
+# Josh
+# Graham
+# Patrick
+# Minh
+# Devon
